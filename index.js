306034dbd84bb6f2fada94aae6224c244b7c418f
Rushil10 <rushil.10.shah@gmail.com>
2021-07-05 12:43:16 +0530

    new

--- index.js
+++ index.js
@@ -18,25 +18,12 @@ app.use(cors())
 app.use(express.json())
 
 const connection = mysql.createConnection({
-    /* host:process.env.DB_HOST,
+    host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
-    charset:'utf8mb4' */
-    host:'database-2.cisu5uvbda7y.ap-south-1.rds.amazonaws.com',
-    user:'admin',
-    password:'password',
-    database:'test_schema',
     charset:'utf8mb4'
 })
-
-/* const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'password',
-    database:'test_schema',
-    charset:'utf8mb4'
-})  */
 
 connection.connect(err => {
     if(err) {
--- util/connect.js
+++ util/connect.js
@@ -2,15 +2,10 @@ const mysql = require('mysql');
 require('dotenv').config()
 
 const connection = mysql.createConnection({
-    /* host:process.env.DB_HOST,
+    host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
-    charset:'utf8mb4' */
-    host:'database-2.cisu5uvbda7y.ap-south-1.rds.amazonaws.com',
-    user:'admin',
-    password:'password',
-    database:'test_schema',
     charset:'utf8mb4'
 })
 
